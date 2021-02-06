5da131b8c879fc11ef0ed83bb17ac83d3735f871
Raghu Alapati <raghu.alapati@gmail.com>
2021-02-06 18:26:01 +0000

    Added DropDown elements to select row and column count for the grid

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # martian-robots
 Martian Robots - React sample test for LME - by Raghu Alapati
+
+
+## Clone the repo & Install dependencies
+
+```
+git clone git@github.com:ralapati/martian-robots.git
+cd martian-robots
+npm install
+```
+
+## Run the project
+````
+npm start 
+````
--- src/Components/gridDimentions.jsx
+++ src/Components/gridDimentions.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const GridDimentions = () => {
 
+    const [rows, updateRows] = useState([0]);
+    const [columns, updateColumns] = useState([0]);
+    const rowsUpdated = (eventKey) => {
+      updateRows(eventKey);
+    };
+
+    const columnsUpdated = (eventKey) => {
+        updateColumns(eventKey);
+    };
 
     return (
         <Container>
@@ -12,15 +22,33 @@ const GridDimentions = () => {
                 Enter size of the grid
             </Row>
             <Row>
-                <Col>Rows</Col>
-                <Col>Columns</Col>
+                <Col>Rows - {rows}</Col>
+                <Col>Columns - {columns}</Col>
             </Row>
             <Row>
                 <Col>
-                    5
+                    <Dropdown onSelect = { (eventKey, event) => rowsUpdated(eventKey, event)}>
+                        <Dropdown.Toggle>
+                            Select Rows
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu style={{height: '200px', overflowY: 'scroll'}}>
+                            {Array.from({length : 50}, (item, index) =>
+                                <Dropdown.Item key={'row' + index} eventKey={index+1}>{index+1}</Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Col>
                 <Col>
-                    10
+                    <Dropdown onSelect={ (eventKey, event) => columnsUpdated(eventKey, event)}>
+                        <Dropdown.Toggle>
+                            Selected Columns
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu style={{height: '200px', overflowY: 'scroll'}}>
+                            {Array.from({length: 50}, (item, index) =>
+                                <Dropdown.Item key={'column'+index} eventKey={index+1}>{index+1}</Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Col>
             </Row>
         </Container>
